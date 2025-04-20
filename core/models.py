ac865b3e161831e9ec60efb34a8e588c9a092f68
solrack1414 <carl.varela@duocuc.cl>
2025-04-20 00:37:34 -0400

    se agrega super usuario y dos tablas, producto y categoria

--- core/models.py
+++ core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name='productos')
+    descripcion = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.nombre
