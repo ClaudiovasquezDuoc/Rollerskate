98fcbeaf6a530504b5116bd756f9ca1300e8ad88
ClaudiovasquezDuoc <cl.vasquezm@duocuc.cl>
2025-05-11 03:30:42 -0400

    Corrige orden de urlpatterns en urls.py

--- RollerSkate/urls.py
+++ RollerSkate/urls.py
@@ -20,8 +20,6 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.conf import settings
 from django.conf.urls.static import static
 
-if settings.DEBUG or settings.ALLOWED_HOSTS:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,3 +27,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+if settings.DEBUG or settings.ALLOWED_HOSTS:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
