44baa1623d3d1eb981a8ac65ec7093875d0ac47a
ClaudiovasquezDuoc <cl.vasquezm@duocuc.cl>
2025-05-11 02:07:15 -0400

    Corrige ALLOWED_HOSTS eliminando https://

--- RollerSkate/settings.py
+++ RollerSkate/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-i+ex_j$*1c4ojxmlr2nv&=cf*hvn21nsgsx^5zqbsspy4fpihp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://rollerskate-tjsj.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['rollerskate-tjsj.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
