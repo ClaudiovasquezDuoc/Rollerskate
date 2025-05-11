b4aa9df49b76ae2ca94abfa55fc362121e3abd21
ClaudiovasquezDuoc <cl.vasquezm@duocuc.cl>
2025-05-11 02:52:56 -0400

    Corrige configuración de archivos estáticos y ajustes en settings.py

--- RollerSkate/settings.py
+++ RollerSkate/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'https://rollerskate-static.onrender.com/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
