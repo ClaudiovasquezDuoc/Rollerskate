2042545ae158fbd8e0d4d4a93acf93d8c6157d5f
ClaudiovasquezDuoc <cl.vasquezm@duocuc.cl>
2025-05-11 03:27:13 -0400

    Configura archivos estáticos y ajustes en settings.py

--- RollerSkate/urls.py
+++ RollerSkate/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG or settings.ALLOWED_HOSTS:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
